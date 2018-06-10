af777da170ee923423f632ad126bf168c720083d
Marskr96 <mareskr524@student.polsl.pl>
2018-06-10 23:28:07 +0200

    Komentarze do Enemy i Map, chyba wszystko?

--- ArcaneInscriber/ArcaneInscriber/Enemy.h
+++ ArcaneInscriber/ArcaneInscriber/Enemy.h
@@ -6,27 +6,48 @@
 #include "Entity.h"
 #include "HasLife.h"
 
+//!Class representing an enemy 
 class Enemy :public sf::Transformable, public sf::Drawable, public Animable, public Physics, public Entity, public HasLife
 {
 public:
+	//!Enum for animation, representing the direction of enemy's movement
 	enum Direction {
 		RIGHT, LEFT
 	};
+	//!Determine if animation is over
+	/*!
+	\return true if animation is over.
+	*/
 	bool canAnimate();
+	//!Determines state of the nenemy (alive, dying or dead)
 	int state;
+	//!Last known position of the enemy before updating
 	sf::Vector2f oldPosition;
+	//!Changes enemys orientation.
+	/*!
+	Mirrors enemys sprite by applying transformation.
+	\param d Direction to set enemys orientation
+	*/
 	void Face(Direction d);
+	//!Starts enemys dying animation and kills it
 	void kaputt();
+	//!Updates sprite to next frame.
 	virtual void Update();
 	Enemy(sf::Vector2f position);
+	//!Actualizes the last know position
 	void actualizePosition(sf::Vector2f pos);
+	//!Checks if the last known position equals actual position
 	bool checkPosition(sf::Vector2f pos);
+	//!Previous frame looking direction
 	Direction lastDir;
+	//!Number of frames the enemy has stayed in the same position
 	int notMoving;
 	~Enemy();
 
 private:
+	//!Keeps texture loaded for future use.
 	sf::Texture* texture;
+	//!Generates sprite from internal data.
 	virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
 };
 
--- ArcaneInscriber/ArcaneInscriber/Map.h
+++ ArcaneInscriber/ArcaneInscriber/Map.h
@@ -8,14 +8,18 @@
 #include "Engine.h"
 #include "Enemy.h"
 
+//!Class representing the map of the game, including background and enemies
 class Map
 {
 private:
+	//!Vector of pointers to Tile, representing layers of background
 	std::vector<Tile*> background;
 public:
 	std::vector<Enemy*> enemies;
+	//!Vector of pointers to Tile, representing all Tiles bulding the map
 	std::vector<Tile*> mapTiles;
 	Map();
+	//!Draws all membrers of Map that should be visible on the screen
 	void Display(sf::Vector2f viewCenter, sf::Vector2f viewSize);
 	~Map();
 };
